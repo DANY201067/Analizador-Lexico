a5d1bbf54657eebedce021d9f8a58855a608026e
DANY201067 <ramirezlimasdanny@gmail.com>
2022-08-14 22:49:58 -0500

    Analizar Lexico Commit

---
+++ AnalizarLexico/src/codigo/Principal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+import java.io.File;
+
+/**
+ *
+ * @author USER
+ */
+public class Principal {
+    public static void main(String[] args) {
+        String ruta = "C:/Users/USER/Documents/NetBeansProjects/AnalizarLexico/src/codigo/Lexer.flex";          
+        generarLexer(ruta);
+    }
+    
+    public static void  generarLexer(String ruta){
+       File archivo = new File(ruta);
+       JFlex.Main.generate(archivo);
+    }
+ 
+}
